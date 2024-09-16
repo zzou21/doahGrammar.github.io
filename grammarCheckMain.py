475b0f5d8c62759a5faa3ed992789a5b8896d964
Jerry Zou <zhihui.zou@duke.edu>
2024-09-16 12:25:28 -0400

    testing grammar error message iteration:

--- grammarCheckMain.py
+++ grammarCheckMain.py
@@ -1,11 +1,26 @@
 import language_tool_python
 
-testText = "I is eating lunch."
-class iteration (test, test, test ,test):
-    def 
+sentence1 = "I is eating lunch."
+sentence2 = "She were going to the market."
+sentence3 = "They has completed their work."
+# class grammarCheck:
+#     def __init__(jsonFilePath, nameDictionary):
 
-
-
+# def grammarChecker(sentence):
     
-def grammarChecker(sentence):
-    
+grammarChecker = language_tool_python.LanguageTool("en-US")
+sentences = [sentence1, sentence2, sentence3]
+
+for sentenceCount, sentenceToCheck in enumerate(sentences, start=1):
+    results = grammarChecker.check(sentenceToCheck)
+    print(f"Error for sentence # {sentenceCount} -- {sentenceToCheck}:")
+    for individualError in results:
+        print(f"RuleId: {individualError.replacements[0]}")
+
+# for index in results:
+#     print(f"ruleId: {index.ruleId}")
+#     print(index)
+
+# print(f"Length of the error is: {len(results)}")
+# print(f"The full error is: {results}")
+# print(f"First index is: {results[0][ruleId]}")
