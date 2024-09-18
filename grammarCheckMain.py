84cba7ab1b3ecc4f9b4a33be5bed5084c05f64c5
Jerry Zou <zhihui.zou@duke.edu>
2024-09-17 22:04:17 -0400

    refined main class object

--- grammarCheckMain.py
+++ grammarCheckMain.py
@@ -1,41 +1,80 @@
 import language_tool_python, json
 from nltk.tokenize import PunktSentenceTokenizer
 
+# Note to developer: do not forget to add work batch during interface design, so that the system automatically updates the correct dictionary routinely because the user cannot check all errors in one sitting. Could update the correct version dictionary after checking each historian's overview.
+
 # json format: dictionary - {"lastName, firstName": [id, "description"]}
 
+# For testing purposes:
 sentence1 = "I is eating lunch."
 sentence2 = "She were going to the market."
 sentence3 = "They has completed their work."
-class grammarCheck:
-    def __init__(self, jsonFilePath, nameDictionary):
+
+class grammarCheckSentenceLevel:
+    def __init__(self, jsonFilePath, nameDictionary, doAHContentSentSegmentedJsonFile):
         self.jsonFilePath = jsonFilePath
         self.nameDict = nameDictionary
+        self.doAHContentSentSegmentedJsonFile = doAHContentSentSegmentedJsonFile
+
         self.DoAHJsonContentDict = None
-        self.DoAHSentSegDict = {}
+        self.DoAHSentSegDict = {} # This dictionary stores the DoAH content that has been sentence-segmented. Format: {"lastName, firstName": [id, "sentence-segmented description"]}
 
     def processJson(self):
-        with open(self.jsonFilePath, "r") as jsonContent:
+        with open(self.jsonFilePath, "r", encoding = "utf-8") as jsonContent:
             self.DoAHJsonContentDict = json.load(jsonContent)
     
     def sentenceSegmentation(self):
         sentSegmentator = PunktSentenceTokenizer()
         for historianString, IdOverviewList in self.DoAHJsonContentDict.items():
-            segmentationResult = sentSegmentator.tokenize(IdOverviewList[1])
-            singleHistorianSentSeg = [singleSentence for singleSentence in segmentationResult]
+            historianIdInt = IdOverviewList[0]
+            historianOverviewString = IdOverviewList[1]
+            segmentationResult = sentSegmentator.tokenize(historianOverviewString)
+            singleHistorianSentSegList = [singleSentence for singleSentence in segmentationResult]
+            # print(singleHistorianSentSeg)
+            self.DoAHSentSegDict[historianString] = [historianIdInt, singleHistorianSentSegList]
+            
+        with open(self.doAHContentSentSegmentedJsonFile, "w", encoding="utf-8") as jsonSegContent:
+            json.dump(self.DoAHSentSegDict, jsonSegContent, ensure_ascii=False, indent = 4)
+
+    
+    # This function and interface check grammar and errors at the sentence-level, using historian overviews that have been sentence-segmented from the "sentenceSegmentation" function.
+    def sentenceGrammarCheck(self):
+        grammarCheckerFunction = language_tool_python.LanguageTool("en-US")
+        for historian, IdOverviewSegmentedList in self.DoAHSentSegDict.items():
+            for Id, segmentedOverview in IdOverviewSegmentedList:
+                for sentence in segmentedOverview:
+                    
+
+        for sentenceCount, sentenceToCheck in enumerate(sentences, start=1):
+            results = grammarChecker.check(sentenceToCheck)
+            print(f"Error for sentence # {sentenceCount} -- {sentenceToCheck}:")
+            for individualError in results:
+                print(f"RuleId: {individualError.replacements[0]}")
+
+    # This helper function is mainly used during code testing to run the entire class object:
+    def operations(self):
+        self.processJson()
+        self.sentenceSegmentation()
+
+
+# class errorCheckWordLevel:
+#     def __init__(self, ):
 
 if __name__ == "__main__":
-    jsonFile = ""
-    nameDictionary = ""
-    grammarCheckMachine = grammarCheck(jsonFile, nameDictionary)
-    print
+    doAHContentJsonFile = "/Users/Jerry/Desktop/DictionaryOfArtHistorians/doahGrammar/doahContentJSON.json"
+    nameDictionary = "/Users/Jerry/Desktop/DictionaryOfArtHistorians/doahGrammar/historianNames.json"
+    doAHContentSentSegmentedJsonFile = "/Users/Jerry/Desktop/DictionaryOfArtHistorians/doahGrammar/doahContentSentSegmentedJSON.json"
+    grammarCheckSentenceLevelMachine = grammarCheckSentenceLevel(doAHContentJsonFile, nameDictionary, doAHContentSentSegmentedJsonFile)
+    grammarCheckSentenceLevelMachine.operations()
+    print(grammarCheckSentenceLevelMachine.DoAHSentSegDict)
 
 #-----------------
 
-grammarChecker = language_tool_python.LanguageTool("en-US")
-sentences = [sentence1, sentence2, sentence3]
+# grammarChecker = language_tool_python.LanguageTool("en-US")
+# sentences = [sentence1, sentence2, sentence3]
 
-for sentenceCount, sentenceToCheck in enumerate(sentences, start=1):
-    results = grammarChecker.check(sentenceToCheck)
-    print(f"Error for sentence # {sentenceCount} -- {sentenceToCheck}:")
-    for individualError in results:
-        print(f"RuleId: {individualError.replacements[0]}")
+# for sentenceCount, sentenceToCheck in enumerate(sentences, start=1):
+#     results = grammarChecker.check(sentenceToCheck)
+#     print(f"Error for sentence # {sentenceCount} -- {sentenceToCheck}:")
+#     for individualError in results:
+#         print(f"RuleId: {individualError.replacements[0]}")
---
+++ tester.py
@@ -0,0 +1,4 @@
+list = [[1,2], [3,4], [5,6]]
+
+for one, two in list:
+    print(f"This is {one}; this is {two}")
