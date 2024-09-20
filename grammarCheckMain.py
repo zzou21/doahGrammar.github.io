2bf418e94278d9b969b8253bc6a0446affe04c11
Jerry Zou <zhihui.zou@duke.edu>
2024-09-19 22:35:37 -0400

    finished printing grammar error location.

--- grammarCheckMain.py
+++ grammarCheckMain.py
@@ -1,14 +1,14 @@
 import language_tool_python, json
 from nltk.tokenize import PunktSentenceTokenizer
 
-# Note to developer: do not forget to add work batch during interface design, so that the system automatically updates the correct dictionary routinely because the user cannot check all errors in one sitting. Could update the correct version dictionary after checking each historian's overview.
+# TO DO: do not forget to add work batch during interface design, so that the system automatically updates the correct dictionary routinely because the user cannot check all errors in one sitting. Could update the correct version dictionary after checking each historian's overview.
 
 # json format: dictionary - {"lastName, firstName": [id, "description"]}
 
-# For testing purposes:
-sentence1 = "I is eating lunch."
-sentence2 = "She were going to the market."
-sentence3 = "They has completed their work."
+# # For testing purposes:
+# sentence1 = "I is eating lunch."
+# sentence2 = "She were going to the market."
+# sentence3 = "They has completed their work."
 
 class grammarCheckSentenceLevel:
     def __init__(self, jsonFilePath, nameDictionary, doAHContentSentSegmentedJsonFile):
@@ -33,28 +33,32 @@ class grammarCheckSentenceLevel:
             # print(singleHistorianSentSeg)
             self.DoAHSentSegDict[historianString] = [historianIdInt, singleHistorianSentSegList]
             
-        with open(self.doAHContentSentSegmentedJsonFile, "w", encoding="utf-8") as jsonSegContent:
-            json.dump(self.DoAHSentSegDict, jsonSegContent, ensure_ascii=False, indent = 4)
-
+        # with open(self.doAHContentSentSegmentedJsonFile, "w", encoding="utf-8") as jsonSegContent:
+            # json.dump(self.DoAHSentSegDict, jsonSegContent, ensure_ascii=False, indent = 4)
     
     # This function and interface check grammar and errors at the sentence-level, using historian overviews that have been sentence-segmented from the "sentenceSegmentation" function.
+
     def sentenceGrammarCheck(self):
+        correctedVersion = {}
         grammarCheckerFunction = language_tool_python.LanguageTool("en-US")
         for historian, IdOverviewSegmentedList in self.DoAHSentSegDict.items():
             for Id, segmentedOverview in IdOverviewSegmentedList:
-                for sentence in segmentedOverview:
+                sentenceCounter = 1
+                for sentenceToCheck in segmentedOverview:
+                    grammarCheckResults = grammarCheckerFunction.check(sentenceToCheck)
+                    for errors in grammarCheckResults:
+                        startSlice = errors.offset
+                        endSlice = errors.offset+errors.errorLength+1
+                        checkLocationSentence = "".join([letter if startSlice <= count < endSlice else "." for count, letter in enumerate(sentenceToCheck)])
+                        print(errors.replacements)
+                        print(f"Error sentence: {sentenceToCheck}")
+                        print(f"Error location: {checkLocationSentence}")
                     
-
-        for sentenceCount, sentenceToCheck in enumerate(sentences, start=1):
-            results = grammarChecker.check(sentenceToCheck)
-            print(f"Error for sentence # {sentenceCount} -- {sentenceToCheck}:")
-            for individualError in results:
-                print(f"RuleId: {individualError.replacements[0]}")
 
     # This helper function is mainly used during code testing to run the entire class object:
     def operations(self):
         self.processJson()
-        self.sentenceSegmentation()
+        # self.sentenceSegmentation()
 
 
 # class errorCheckWordLevel:
@@ -64,9 +68,10 @@ if __name__ == "__main__":
     doAHContentJsonFile = "/Users/Jerry/Desktop/DictionaryOfArtHistorians/doahGrammar/doahContentJSON.json"
     nameDictionary = "/Users/Jerry/Desktop/DictionaryOfArtHistorians/doahGrammar/historianNames.json"
     doAHContentSentSegmentedJsonFile = "/Users/Jerry/Desktop/DictionaryOfArtHistorians/doahGrammar/doahContentSentSegmentedJSON.json"
-    grammarCheckSentenceLevelMachine = grammarCheckSentenceLevel(doAHContentJsonFile, nameDictionary, doAHContentSentSegmentedJsonFile)
+    smallScaleTestJson = "/Users/Jerry/Desktop/DictionaryOfArtHistorians/doahGrammar/smalldoahContentSentSegPractice.json"
+    grammarCheckSentenceLevelMachine = grammarCheckSentenceLevel(smallScaleTestJson, nameDictionary, doAHContentSentSegmentedJsonFile)
     grammarCheckSentenceLevelMachine.operations()
-    print(grammarCheckSentenceLevelMachine.DoAHSentSegDict)
+    # print(grammarCheckSentenceLevelMachine.DoAHSentSegDict)
 
 #-----------------
 
--- tester.py
+++ tester.py
@@ -1,4 +1,36 @@
-list = [[1,2], [3,4], [5,6]]
+'''This file is intended to test functions only. Use this as scratch paper.'''
+# list = [[1,2], [3,4], [5,6]]
 
-for one, two in list:
-    print(f"This is {one}; this is {two}")
+# for one, two in list:
+#     print(f"This is {one}; this is {two}")
+import language_tool_python, json
+from nltk.tokenize import PunktSentenceTokenizer
+
+grammarChecker = language_tool_python.LanguageTool("en-US")
+
+sentences = "Authority in Byzantine, Medieval, Russian, and primitive decorative arts; curator. Ross was born to William Edwin Ross (1870-1945) and Mary Katherine Rogers (Ross) (1875-1939) in Moriches, New York. He studied at the University of Berlin during the summer of 1927. Ross received an A.B. in 1928 and an M.A. in 1930 from Harvard University. During his M.A., he worked as an instructor in Fine Arts at the University of Pittsburgh (1928-1929). In 1930, he began his Ph.D. under <a href=\"/chaseg\">George H. Chase</a> on Romanesque enamels, presumably left unfinished, as often happened at that time. He also studied at the Centro de Estudios Históricos in Madrid (1930), and New York University (1933-34, 1937). Ross was as an assistant at the Brooklyn Museum in 1934. The same year, he joined the Walters Art Gallery (now the Walters Art Museum) in Baltimore as a research assistant, becoming the Associate Curator of Byzantine and Medieval Art in 1937, later adding Decorative Arts.\n\nDuring World War II, Ross first enlisted in the U.S. Marine Corps in 1942 and then was selected by <a href=\"https://arthistorians.info/webbg/\">Geoffrey Webb</a> to assist with administrative work for the Monuments, Fine Arts, and Archives (MFAA) Section, known as the Monuments Men. In 1944, Ross discovered the panels of the famous Isenheim Altarpiece by Matthias Grünewald in occupied Alsace. Upon returning to the United States, Ross resumed his role as Associate Curator at the Walters. In 1947, he organized the first major exhibition of Early Christian and Byzantine Art in the U.S. at the Baltimore Museum of Art.\n\nHe moved to California in 1952 to head the Art Department at the Los Angeles County Museum of Art (LACMA). Ross collaborated with <a href=\"https://arthistorians.info/kinge/\">Edward S. King</a> on a book, <i>Catalogue of the American Works of Art: including French Medals made for America [in] the Walters Art Gallery</i>, which was published in 1956. In 1958, art collector Marjorie Merriweather Post (1887-1973) appointed him Chief Curator of her collection of eighteenth-century French and Imperial Russian at Post's private art collection and grounds, Hillwood, Washington, D.C.\n\nRoss was a Fulbright Lecturer on Byzantine art at the Kunsthistorisches Institute at the University of Vienna (1960-1961). He authored the first two volumes of the <i>Catalogue of the Byzantine and Early Medieval Antiquities</i> at Dumbarton Oaks, published in 1962 and 1965, covering metalwork, ceramics, glass, jewelry, enamels, and glyptics. <a href=\"https://arthistorians.info/weitzmannk/\">Kurt Weitzmann</a> authored the third volume, focusing on ivories and steatites. In 1965, Ross spent the rest of his life at the Virginia Museum of Fine Arts in Richmond, devoting himself to making the museum’s collection of medieval and Byzantine art one of the best in the United States. That same year, he published <i>The Art of Karl Fabergé and His Contemporaries</i>, a catalog of the Post collection, followed by <i>Russian Porcelains</i> in 1969. Ross died in 1977, leaving two books and a catalog for an upcoming exhibition at the Metropolitan Museum of Art unfinished. Ross's wife, Lotus Bobb (Ross) (1893-1969), was a Broadway actress.\n\nRoss made groundbreaking contributions to Byzantine, Medieval art and Russian art. His work <i>Catalogue of the Byzantine and Early Medieval Antiquities at Dumbarton Oaks</i> was \"a model of its kind\" (Beckwith). In addition, <i>The Art of Karl Fabergé and His Contemporaries </i>further the discourse on lesser-known Russian artists such as Fabergé, despite Sprague's criticism of the book as a \"glorified catalogue of part of the personal collection of Mrs. Marjorie Merriweather Post.\" Ross's summaries \"are admirably succinct, and his authority is marked throughout.\"(Beckwith)\n\nRoss was a Guggenheim fellow four times, a member of the American Association of Museums, the Archaeological Institute of America, and the committee of Hammond-Harwood House."
+
+
+# sentSegmentator = PunktSentenceTokenizer()
+# segmentationResult = sentSegmentator.tokenize(sentences)
+
+# for e in segmentationResult:
+#     print(e)
+
+
+sentenceTestShort = ["I is eating lunch.", "She were going to the market", "They has completed their work."]
+
+for sentenceCount, sentenceToCheck in enumerate(sentenceTestShort, start=1):
+    results = grammarChecker.check(sentenceToCheck)
+    for errors in results:
+        startSlice = errors.offset
+        endSlice = errors.offset+errors.errorLength+1
+        checkLocationSentence = "".join([letter if startSlice <= count < endSlice else "." for count, letter in enumerate(sentenceToCheck)])
+        print(errors.replacements)
+        print(f"Error sentence: {sentenceToCheck}")
+        print(f"Error location: {checkLocationSentence}")
+
+    # print(f"Error for sentence # {sentenceCount} -- {sentenceToCheck}:")
+    # for individualError in results:
+        # print(f"RuleId: {individualError.replacements[0]}")
+
